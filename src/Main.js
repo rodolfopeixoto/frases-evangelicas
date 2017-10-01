e1572897333c0276bbcb0eeb4dbf0d4f5a9ee6ed
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-10-01 16:38:56 -0300

    Created button

--- src/Main.js
+++ src/Main.js
@@ -8,46 +8,38 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-
+const btnPress = () => {
+    alert('Botão pressionado');
+  }
 
 const styles = StyleSheet.create({ 
     main: {
-      flex: 1, //ocupa 1:1 um total de 1 par 1
-      backgroundColor: 'cornflowerblue'
+      paddingTop: 40
     },
-    header:{
-      flex: 2, //ocupa 2:11 um total de 2 para 11
-      backgroundColor: 'brown'
-    },
-    content:{
-      flex: 8, //ocupa 8:11 um total de 8 para 11
-      backgroundColor: 'darkblue'
-    },
-    footer: {
-      flex: 1, //ocupa 1:11 um total de 1 para 11
-      backgroundColor: 'orangered'
-    }
 });
 
 export default class frases extends Component {
+  
+
+
   render() {
 
-    const { main, header, content, footer } = styles;
+    const { main } = styles;
 
     return (
       <View style={main}>
-        <Text style={header}>
-          HEADER
-        </Text>
-        <Text style={content}>
-          CONTENT
-        </Text>
-        <Text style={footer}>
-          FOOTER
-        </Text>
+        <Text>Olá, Rodolfo Peixoto</Text>
+        <Text>O Aplicativo de mensagens</Text>
+        <Button 
+        onPress={btnPress}
+         color='blue'
+          title="Visualizar mensagem"
+          accessibilityLabel="Clique para abrir as notícias"
+        />
       </View>
     );
   }
