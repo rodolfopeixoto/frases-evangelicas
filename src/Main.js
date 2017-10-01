d1633a4d74b927842d19e475e0d89a67a9ddc64b
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-10-01 16:11:25 -0300

    Add proportion of the flexbox

--- src/Main.js
+++ src/Main.js
@@ -13,61 +13,40 @@ import {
 
 
 
-const styles = StyleSheet.create({
-  TextStyle: {
-    fontSize: 40,
-    backgroundColor: 'blue',
-    paddingTop: 15,
-    color: 'white',
-    textDecorationLine: 'underline', // underline, line-through, underline line-through 
-    height: 60,
-    width: 60,
-    textAlign: 'center'
-    // textAlign: 'center'
-    // fontWeight: '900' // values 100 a 900
-    // fontStyle: 'italic'
-    // shadowColor: '#000',
-    // shadowOffset: { width: 0, height: 2},
-    // shadowOpacity: 0.4
-  },
-  TextStyle2: {
-    fontSize: 40,
-    backgroundColor: 'red',
-    paddingTop: 15,
-    color: 'white',
-    textDecorationLine: 'underline', // underline, line-through, underline line-through 
-    height: 60,
-    width: 60,
-    textAlign: 'center'
-    // textAlign: 'center'
-    // fontWeight: '900' // values 100 a 900
-    // fontStyle: 'italic'
-    // shadowColor: '#000',
-    // shadowOffset: { width: 0, height: 2},
-    // shadowOpacity: 0.4
-  },
-  viewStyle: {
-    backgroundColor: 'skyblue',
-    height: 300,
-    //flex-start, center, flex-end, space-around, space-between
-    justifyContent: 'space-around',   // values: flex-end, center,  
-    alignItems: 'center',
-    flexDirection: 'row'
-  }
+const styles = StyleSheet.create({ 
+    main: {
+      flex: 1, //ocupa 1:1 um total de 1 par 1
+      backgroundColor: 'cornflowerblue'
+    },
+    header:{
+      flex: 2, //ocupa 2:11 um total de 2 para 11
+      backgroundColor: 'brown'
+    },
+    content:{
+      flex: 8, //ocupa 8:11 um total de 8 para 11
+      backgroundColor: 'darkblue'
+    },
+    footer: {
+      flex: 1, //ocupa 1:11 um total de 1 para 11
+      backgroundColor: 'orangered'
+    }
 });
 
 export default class frases extends Component {
   render() {
 
-    const { TextStyle, viewStyle } = styles;
+    const { main, header, content, footer } = styles;
 
     return (
-      <View style={viewStyle}>
-        <Text style={styles.TextStyle}>
-          A
+      <View style={main}>
+        <Text style={header}>
+          HEADER
         </Text>
-        <Text style={styles.TextStyle2}>
-          B
+        <Text style={content}>
+          CONTENT
+        </Text>
+        <Text style={footer}>
+          FOOTER
         </Text>
       </View>
     );
