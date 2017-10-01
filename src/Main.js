1d84c125703a21f78858ace80bfe44c397139f20
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-10-01 15:59:07 -0300

    Modified position with flexbox

--- src/Main.js
+++ src/Main.js
@@ -6,48 +6,70 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+
+
+const styles = StyleSheet.create({
+  TextStyle: {
+    fontSize: 40,
+    backgroundColor: 'blue',
+    paddingTop: 15,
+    color: 'white',
+    textDecorationLine: 'underline', // underline, line-through, underline line-through 
+    height: 60,
+    width: 60,
+    textAlign: 'center'
+    // textAlign: 'center'
+    // fontWeight: '900' // values 100 a 900
+    // fontStyle: 'italic'
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2},
+    // shadowOpacity: 0.4
+  },
+  TextStyle2: {
+    fontSize: 40,
+    backgroundColor: 'red',
+    paddingTop: 15,
+    color: 'white',
+    textDecorationLine: 'underline', // underline, line-through, underline line-through 
+    height: 60,
+    width: 60,
+    textAlign: 'center'
+    // textAlign: 'center'
+    // fontWeight: '900' // values 100 a 900
+    // fontStyle: 'italic'
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2},
+    // shadowOpacity: 0.4
+  },
+  viewStyle: {
+    backgroundColor: 'skyblue',
+    height: 300,
+    //flex-start, center, flex-end, space-around, space-between
+    justifyContent: 'space-around',   // values: flex-end, center,  
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
+});
+
 export default class frases extends Component {
   render() {
+
+    const { TextStyle, viewStyle } = styles;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View style={viewStyle}>
+        <Text style={styles.TextStyle}>
+          A
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+        <Text style={styles.TextStyle2}>
+          B
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('frases', () => frases);
